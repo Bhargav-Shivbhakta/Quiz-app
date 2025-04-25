6487e38245eeaf0dfacfbd1456cd92ac7a4389e0
BUS <bhargavshivbhakta@gmail.com>
2025-04-25 10:27:03 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
 import streamlit as st
 
-# ✅ Use full URI from secrets
+# Load the URI from secrets
 MONGO_URI = st.secrets["mongo_uri"]
 
 client = MongoClient(MONGO_URI)
-
 db = client["quiz_app"]
+
 users_col = db["users"]
 quizzes_col = db["quizzes"]
 responses_col = db["responses"]
